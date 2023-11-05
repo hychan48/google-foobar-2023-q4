beae06387e75f626e59e81c69d450d46c295fb2e
JC <jchan48@gmail.com>
2023-11-05 01:04:09 -0500

    conftest.py fixed the path issue in github actions

---
+++ conftest.py
@@ -0,0 +1,9 @@
+import sys
+from pathlib import Path
+
+# for fixing path in docker / github actions
+
+# Calculate the directory of your package and add it to sys.path
+# repo_dir = Path(__file__).parent.parent  # Adjust depending on `conftest.py` location
+repo_dir = Path(__file__).parent  # Adjust depending on `conftest.py` location
+sys.path.insert(0, str(repo_dir))
